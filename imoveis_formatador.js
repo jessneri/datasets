dc299e19e44c2da52e3d3fcccce283c38437cfb1
Jéssica Neri <jess.neri94@gmail.com>
2020-08-23 10:48:48 -0300

    Documentação: tratamento de dados feito.

--- imoveis_formatador.js
+++ imoveis_formatador.js
@@ -8,6 +8,28 @@ const stream = fs.createReadStream("houses.csv");
 
 let conteudo = [];
 stream.pipe(csvParser()).on("data", (data) => { //pipe: cada linha sendo entendida como objeto
-    conteudo += data;
-    console.log("Conteúdo", data);
+    conteudo.push({
+        city: data['city'].trim(),
+        area: Number(data['area']),
+        rooms: Number(data['rooms']),
+        bathroom: Number(data['bathroom']),
+        'parking spaces': Number(data['parking spaces']),
+        floor: Number(data['floor']),
+        animal: Boolean(data['animal']),
+        furniture: Boolean(data['furniture']),
+        hoa: Number(data['hoa']),
+        'rent amount': Number(data['rent amount']),
+        'property tax': Number(data['property tax']),
+        'fire insurance': Number(data['fire insurance']),
+        total: Number(data['total'])
+    });
+    //city,area,rooms,bathroom,parking spaces,floor,animal,furniture,hoa,
+    //rent amount,property tax,fire insurance,total
 });
+
+stream.on('end', () => { //o 'end' não tem argumento
+    console.log("Conteúdo", conteudo)
+})
+
+
+//node imoveis_formatador.js
